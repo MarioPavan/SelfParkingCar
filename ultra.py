27056daec8496b2c521c45e78fce4efbcf6f4b06
EDLMM <lilililyzs@outlook.com>
2018-12-11 14:53:58 +0100

    add button interrupt in ultrasonic sensor test

--- ultra.py
+++ ultra.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 from ev3dev2.sensor.lego import UltrasonicSensor
 from ev3dev2.led import Leds
+from ev3dev2.button import Button
+
 from time import sleep
 
 us = UltrasonicSensor()
 leds = Leds()
-
 leds.all_off() # stop the LEDs flashing (as well as turn them off)
 
-if us.distance_centimeters < 10: # to detect objects closer than 40cm
-    # In the above line you can also use inches: us.distance_inches < 16
-    leds.set_color('LEFT',  'RED')
-    leds.set_color('RIGHT', 'RED')
-else:
-    leds.set_color('LEFT',  'GREEN')
-    leds.set_color('RIGHT', 'GREEN')
+running=True
+btn = Button()
 
-sleep (0.01) # Give the CPU a rest
+while(running):
+    if btn.any():
+        running=False
+    if us.distance_centimeters < 10: # to detect objects closer than 40cm
+        # In the above line you can also use inches: us.distance_inches < 16
+        leds.set_color('LEFT',  'RED')
+        leds.set_color('RIGHT', 'RED')
+    else:
+        leds.set_color('LEFT',  'GREEN')
+        leds.set_color('RIGHT', 'GREEN')
+
+    sleep (0.01) # Give the CPU a rest
